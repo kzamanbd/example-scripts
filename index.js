d6bf730dd59cba79f4e80ed5f37238049c6bb6ee
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-06-07 16:20:04 +0600

    fixed on load map issue

--- index.js
+++ index.js
@@ -335,21 +335,4 @@ function initMap() {
 	});
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-	directionsService
-		.route({
-			origin: {
-				query: document.getElementById('start').value
-			},
-			destination: {
-				query: document.getElementById('end').value
-			},
-			travelMode: google.maps.TravelMode.DRIVING
-		})
-		.then((response) => {
-			directionsRenderer.setDirections(response);
-		})
-		.catch((e) => window.alert('Directions request failed due to ' + status));
-}
-
 window.initMap = initMap;
